3ae9053cadfa1d1423f887ba6a41f5af68da81d8
Manoela Cassia <manoelacs@ic.ufal.br>
2021-02-24 18:45:45 -0300

    component countdown

--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import CompleteChallenges from '../components/CompletedChallenges';
+import Countdown from '../components/CountDown';
 import ExperienceBar from '../components/ExperienceBar'
 import Profile from '../components/Profile';
 import styles from '../styles/pages/Home.module.css';
@@ -7,11 +8,15 @@ import styles from '../styles/pages/Home.module.css';
 export default function Home() {
   return (    
     <div className={styles.container}>
+      <Head>
+        <title>Início | Move.it</title>
+      </Head>
        <ExperienceBar/>
       <section>
         <div>
           <Profile/>
           <CompleteChallenges/>
+          <Countdown/>
         </div>
 
         <div>
